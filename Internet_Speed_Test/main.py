ee436546b6a54584dead57a5b755ed9d7636ca7c
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2020-11-23 00:55:42 +0530

    Update main.py

--- Internet_Speed_Test/main.py
+++ Internet_Speed_Test/main.py
@@ -1,1 +1,6 @@
+import speedtest
 
+st = speedtest.Speedtest()
+print("Your download speed is",st.download()//10**6, "Mbps")
+print("Your upload speed is",st.upload()//10**6, "Mbps")
+print("Your ping speed is",int(st.results.ping),"MS")
